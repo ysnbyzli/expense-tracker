f0f91e8371985b99cefaac19cf84e882f926e632
ysnbyzli <yasinbeyazli29@gmail.com>
2021-09-01 12:50:54 +0300

    redux kurulum

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,19 @@
 import ReactDOM from 'react-dom';
-import App from './App';
 import 'antd/dist/antd.css';
+import { applyMiddleware, createStore } from 'redux';
+import thunk from 'redux-thunk';
+
+import App from './App';
+import rootReducer from './store';
+import { Provider } from 'react-redux';
+
+const store = createStore(rootReducer, applyMiddleware(thunk))
+
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
 
